7734528753a2ba79697fdd27593ff574c457fe5c
jramos-tech <jramos@student.neumont.edu>
2023-06-26 14:51:48 -0600

    Add project files.

---
+++ Source/Game/GamerModeActivated/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+#ifdef _DEBUG
+	std::cout << "debug\n";
+#endif
+	std::cout << "Hello everybody my name is Markiplier and welcome back to another Five nights at Freddy";
+}
